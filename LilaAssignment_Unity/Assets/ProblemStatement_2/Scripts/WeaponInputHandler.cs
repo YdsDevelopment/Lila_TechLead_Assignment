f21ae470c8cfe5abc0d3869bfb37d3dc6c5dbd2d
Pavan Chavan <pavan.chavan_ext@veikkaus.fi>
2026-07-24 11:00:03 +0530

    + Weapon input Handler is Added

---
+++ LilaAssignment_Unity/Assets/ProblemStatement_2/Scripts/WeaponInputHandler.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace TicTacToe.WeaponSystem
+{
+    public sealed class WeaponInputHandler : MonoBehaviour
+    {
+        [SerializeField] private KeyCode primary1Key = KeyCode.Alpha1;
+        [SerializeField] private KeyCode primary2Key = KeyCode.Alpha2;
+        [SerializeField] private KeyCode secondaryKey = KeyCode.Alpha3;
+        [SerializeField] private KeyCode reloadKey = KeyCode.R;
+        [SerializeField] private int fireMouseButton = 0;
+
+        private PlayerWeaponController _controller;
+
+        private void Awake()
+        {
+            _controller = new PlayerWeaponController();
+        }
+
+        private void Update()
+        {
+            PollInput();
+            _controller.Tick(Time.deltaTime, Time.time);
+        }
+
+        private void PollInput()
+        {
+            if (Input.GetKeyDown(primary1Key))
+                _controller.SwitchWeapon(WeaponSlot.Primary1);
+
+            if (Input.GetKeyDown(primary2Key))
+                _controller.SwitchWeapon(WeaponSlot.Primary2);
+
+            if (Input.GetKeyDown(secondaryKey))
+                _controller.SwitchWeapon(WeaponSlot.Secondary);
+
+            if (Input.GetMouseButton(fireMouseButton))
+                _controller.Fire(Time.time);
+
+            if (Input.GetKeyDown(reloadKey))
+                _controller.Reload(Time.time);
+        }
+    }
+}
